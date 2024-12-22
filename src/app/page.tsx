55cd75baf486226e610dc9cbd431db63de9307a1
m19e <m19e.dev@gmail.com>
2024-12-22 23:43:09 +0900

    Demo button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
@@ -47,6 +48,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button>Shadcn/ui Button</Button>
         </div>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
