eb94e034c5ae9fe19cc3a9e55b64d8603452e3bc
m19e <m19e.dev@gmail.com>
2024-12-25 01:11:48 +0900

    Check transformer-demo working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 
 import { Canvas } from '@/components/Canvas'
 import { Picker } from '@/components/EmojiPickerReact'
+import { Demo } from '@/components/TransformerDemo'
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8">
+        <Demo />
         <Canvas />
         <Picker />
       </main>
