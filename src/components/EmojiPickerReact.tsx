c9cb4d6e744c137b6eb0284c28a7710cb8c1dfc1
m19e <m19e.dev@gmail.com>
2024-12-24 18:37:07 +0900

    Print svg url by click emoji

--- src/components/EmojiPickerReact.tsx
+++ src/components/EmojiPickerReact.tsx
@@ -1,7 +1,10 @@
 'use client'
 import dynamic from 'next/dynamic'
 
-import { Categories, EmojiStyle } from 'emoji-picker-react'
+import { EmojiStyle } from 'emoji-picker-react'
+
+import { EPR_CATEGORIES_JA } from '@/constants'
+import { getSvgUrl } from '@/tools'
 
 const EmojiPicker = dynamic(() => import('emoji-picker-react'), {
   ssr: false,
@@ -11,23 +14,14 @@ export const Picker = () => {
   return (
     <EmojiPicker
       emojiStyle={EmojiStyle.TWITTER}
-      onEmojiClick={(e) => console.log(e)}
+      categories={EPR_CATEGORIES_JA}
+      onEmojiClick={(e) => console.log(getSvgUrl(e.unifiedWithoutSkinTone))}
+      skinTonesDisabled
       searchPlaceholder="検索"
       previewConfig={{
         defaultEmoji: '1f60e',
         defaultCaption: '今の気分はどう？',
       }}
-      categories={[
-        { category: Categories.SUGGESTED, name: 'よく使う絵文字' },
-        { category: Categories.SMILEYS_PEOPLE, name: 'スマイリーと人' },
-        { category: Categories.ANIMALS_NATURE, name: '動物と自然' },
-        { category: Categories.FOOD_DRINK, name: 'フードとドリンク' },
-        { category: Categories.TRAVEL_PLACES, name: '旅行と場所' },
-        { category: Categories.ACTIVITIES, name: 'アクティビティ' },
-        { category: Categories.OBJECTS, name: 'オブジェクト' },
-        { category: Categories.SYMBOLS, name: '記号' },
-        { category: Categories.FLAGS, name: '旗' },
-      ]}
     />
   )
 }
